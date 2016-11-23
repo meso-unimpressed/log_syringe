bbc58a866a6d5fabec544017a52c1fcb13acaa45
Joakim Reinert <mail@jreinert.com>
2016-11-23 15:56:57 +0100

    add .define method to main module

--- lib/loggify.rb
+++ lib/loggify.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 require 'loggify/version'
+require 'loggify/dsl'
 
 # Top level namespace
 module Loggify
+  # Define logging for a class/module
+  # @param klass [Module] The class or module to define logging for
+  def self.define(klass, &block)
+    DSL.new(klass, &block)
+  end
 end
--- spec/loggify_spec.rb
+++ spec/loggify_spec.rb
@@ -6,7 +6,29 @@ describe Loggify do
     expect(Loggify::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  describe '.define' do
+    let(:test_class) { Class.new }
+    let(:dsl) { instance_double(Loggify::DSL) }
+
+    before do
+      allow(Loggify::DSL).to receive(:new) do |&block|
+        dsl.instance_eval(&block)
+        dsl
+      end
+    end
+
+    it 'passes the given class to the dsl' do
+      described_class.define(test_class) {}
+      expect(Loggify::DSL).to have_received(:new).with(test_class)
+    end
+
+    it 'instance evals with the dsl instance' do
+      context = nil
+      described_class.define(test_class) do
+        context = self
+      end
+
+      expect(context).to be(dsl)
+    end
   end
 end
